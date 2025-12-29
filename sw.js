8a03e945d58af1659337fd3a9e3eaa97de0baf69
Matej Hron <matejhron@Matejs-MacBook-Pro.local>
2025-12-29 22:35:31 +0100

    Fix anatomy quiz: update to 100 questions, add dropdown options, bump version to 0.0.31

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Deco Theory PWA
-const CACHE_NAME = 'deco-theory-0.0.30';
+const CACHE_NAME = 'deco-theory-0.0.31';
 
 // Files to cache for offline use
 const STATIC_ASSETS = [
